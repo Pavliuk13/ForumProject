ef58d0029db019d3a88e6072670d5be81de04bfe
Vasiliy <vasiokpavluyk@gmail.com>
2022-01-23 21:38:10 +0200

    start project

---
+++ Models/AppDBContext/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumProject.Models.AppDBContext
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
