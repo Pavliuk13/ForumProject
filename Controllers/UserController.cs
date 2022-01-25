53fbf7303c99c78a6304c7fcfffcbe4e7bff394a
Vasiliy <vasiokpavluyk@gmail.com>
2022-01-25 16:45:21 +0200

    Administration panel added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ForumProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() => View(_userManager.Users.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser() {Email = model.Email, UserName = model.Email};
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+            
+            EditUserViewModel model = new EditUserViewModel {Id = user.Id, Email = user.Email};
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.UserName = model.Email;
+                    user.Email = model.Email;
+                    
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ForumProject.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
