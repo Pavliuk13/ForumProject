7690fd60e9035a27385e6faf2d7ccec6883e7066
Vasiliy <vasiokpavluyk@gmail.com>
2022-02-06 00:22:01 +0200

    'edit' and 'delete' actions was added

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -19,6 +19,7 @@ namespace ForumProject.Controllers
             return View(_db.Categories.ToList());
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -33,6 +34,56 @@ namespace ForumProject.Controllers
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
             }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id < 0)
+                return NotFound();
+            
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+        
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id < 0)
+                return NotFound();
+            
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
             return RedirectToAction("Index");
         }
     }
